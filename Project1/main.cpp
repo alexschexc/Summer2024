7bc78865ed888d80f79b8ae725305c523d33126f
Alex Schexnayder <alex.schex@gmail.com>
2024-06-24 23:38:13 -0500

    refactored functions

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-//Added math library
 #include <cmath>
 #include <string>
 #include <unistd.h>
@@ -8,16 +7,13 @@
 
 using namespace std;
 
-//Since you already did namespace std; it should just be string not std::string
 inline bool file_exists(const string& name);
-//Changed parameters to int id, int p, and int i based on pseudocode found in PDF
 void hAndSAlgo(int id, int p, int i);
 bool barrier(int arr[], int n);
 
 
-int main()
-{
-//1. Create variable and take in arguments
+int main() {
+  //1. Create variable and take in arguments
   string A = ""; // Input File name
   string B = ""; // Output File Name
   int n = 0; // num of input elements
@@ -32,11 +28,6 @@ int main()
 
   cout << "Output File Name?: ";
   cin >> B;
-  //Since it is an output file, it will not exist initially, removing the block of code
-  /*if(!file_exists(B)){
-    cout << "ERROR File does not exist!";
-    exit(0);
-  }*/
 
   cout << "Number of elements being put in Array?: ";
   cin >> n;
@@ -52,9 +43,9 @@ int main()
    cout << "ERROR value less than 0 (zero)";
    exit(0); 
   }
+  
 
-
-//2. populate entry array
+  //2. populate entry array
   int arr[p];
   string line = "";
   ifstream newFile(A);
@@ -72,24 +63,20 @@ int main()
 
   // arr[] should now be populated
 
-//3. generate m processes to handle the algorithm
-
-
+  //3. generate m processes to handle the algorithm
   
-//4. run the algorithms
+  //4. run the algorithms
+
+  //5. output results
+  return 0;
 
 
-//5. output results
-
-
+}
 inline bool file_exists (const string& name) {
   struct stat buffer;   
   return (stat (name.c_str(), &buffer) == 0); 
 }
 
-
-  return 0;
-}
 //*6. Hillis and Steele Algorithm
 void hAndSAlgo (int id, int p, int i){
   
@@ -97,7 +84,7 @@ void hAndSAlgo (int id, int p, int i){
 
 //*7. Barrier Algorithm
 //Method should be called arriveAndWait(), should be void and blocks the process
-void arriveAndWait()
+void arriveAndWait(){
 /*bool barrier(int arr[], int n){
   const int x = n;
   bool guardTwo[x] = {false} ;
@@ -105,5 +92,4 @@ void arriveAndWait()
 
   return guardTwo[];
 }*/
-
-
+  }
