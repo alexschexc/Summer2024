76df72abee31b8414da73cf256366ac034ec270d
Alex Schexnayder <alex.schex@gmail.com>
2024-06-26 17:33:20 -0500

    fixed array assignment, shared memory block created

--- Project1/my-sum.cpp
+++ Project1/my-sum.cpp
@@ -4,14 +4,23 @@
 #include <string>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <stdio.h>
+#include <bits/stdc++.h> 
+
 
 using namespace std;
 
 inline bool file_exists(const string& name);
 void hAndSAlgo(int id, int p, int i);
 bool barrier(int arr[], int n);
+const int MEMORY_SIZE = 4096;
+const int BUFFER_SIZE = 256;
+void errormsg( char *msg );
 
-int main() {
+int main(void) {
   //1. Create variable and take in arguments
   string A = ""; // Input File name
   string B = ""; // Output File Name
@@ -51,24 +60,38 @@ int main() {
   int x;
   if(newFile.is_open())
     {
-      while (cnt < arr[p] && newFile >> x)
+      string line;
+      while (getline(newFile, line))
 	{
+	 x = stoi(line);
 	 arr[cnt++] = x;
 	}
       newFile.close();
     }
   else cout << "Unable to open file";
   // arr[] should now be populated
+  // this has been successfully tested with for loop cout output
+
 
   //3. generate m processes to handle the algorithm
+  int segment_id = 0;
 
-  
+  char buffer[BUFFER_SIZE];
+
+  segment_id = shmget( IPC_PRIVATE, MEMORY_SIZE, S_IRUSR | S_IWUSR );
+
+  if ( segment_id < 0 ) errormsg( "ERROR in creating a shared memory segment\n" );
+
+  fprintf( stdout, "Segment id = %d\n", segment_id );
   //4. run the algorithms
 
   //5. output results
   return 0;
 }
-
+void errormsg( char *msg ) {
+   perror( msg );
+   exit( 1 );
+}
 inline bool file_exists (const string& name) {
   struct stat buffer;   
   return (stat (name.c_str(), &buffer) == 0); 
